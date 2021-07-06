a5a9ab871bba29799d699572e161ba50e499e62e
Kajanan98 <63833938+Kajanan98@users.noreply.github.com>
2021-07-06 06:18:17 +0530

    temp

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const userController = require ('../controllers/User')
 //admin dashboard
 router.get('/', async (req, res) => {
     var ob = await teacher.find()
@@ -12,11 +13,20 @@ router.get('/test', async (req, res) => {
     //var total = ob.length
     res.render('admin/', { result: [] })
 })
+router.get('/newUser', async (req, res) => {
+    //var ob  = await teacher.find()
+    //var total = ob.length
+    res.render('admin/newUser', { title:'New User',data:false })
+})
 
 router.get('/addTeacher', async (req, res) => {
     //var ob  = await teacher.find()
     //var total = ob.length
     res.render('admin/newUser', { title: 'New Teacher', data: [], brands: [] })
 })
+
+router.get('/editUser/:id',userController.findOne)
+router.post('/newUser/new',userController.createUser);
+router.put('/editUser/editSubmit/:id',userController.updateUser);
 
 module.exports = router
